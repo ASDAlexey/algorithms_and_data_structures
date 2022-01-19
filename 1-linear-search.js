be7dd929c2d44570c40afac9bb1c4629b6773e79
alexey popov <alexey.popov@5thkind.com>
2022-01-19 23:21:16 +0300

    add binary search

--- 1-linear-search.js
+++ 1-linear-search.js
@@ -1,5 +1,6 @@
 const array = [1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11];
 let count = 0;
+
 // O(n)
 const linearSearch = (arr, item) => {
   for (let i = 0; i < arr.length; i++) {
@@ -13,5 +14,4 @@ const linearSearch = (arr, item) => {
 };
 
 console.log(linearSearch(array, 11)); // element index in array in current case 3
-// console.log(`count = ${ count }`)
 console.log(`count = ${count}`);
